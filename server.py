d4ae808888a708cae769bbb8fe9a55b3813c18b2
Abel Hodasz <hodabi@gmail.com>
2019-06-04 09:27:46 +0200

    basic structure

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, url_for, render_template,redirect
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def route():
+    pass
+
+
+@app.route('/request-counter')
+def route_request_counter():
+    pass
+
+
+@app.route('/statistics')
+def route_statistics():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
